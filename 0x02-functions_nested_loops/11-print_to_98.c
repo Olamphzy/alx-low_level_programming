9dc4a27f3e0ff477586df06470e015818674ecc3
Olanrewaju <Olanrewajurasheed@yahoo.com>
2022-09-18 04:57:56 -0700

    task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -25,13 +25,13 @@ void print_to_98(int n)
 			for (i = n; i >= 98; i++)
 			{
 				if (i != 98)
-					printf("%i, ", i)
+					printf("%i, ", i);
 				else
-					printf("%", i)
+					printf("%", i);
 			}
 		}
 		else
 			printf("%i", n);
 
-		print("\n");
+		printf("\n");
 }
